5b17089bdb596cfc237c8992c73814a45e004298
Zayahan Hasan Shah <zayahanshah.18@gmail.com>
2024-04-23 22:38:40 +0500

    update the submit button

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -29,7 +29,7 @@ const Contact = () => {
     <>
       <div id="contact" className="h-[screen] max-w-[1040px] m-auto md:pl-20 p-4 py-16">
         <h1 className="py-4 text-4xl text-center font-bold text-[#001b5e]">Contact</h1>
-        <form  ref={form} onSubmit={sendEmail} onClick={notify} className="p-4 px-8 m-auto mt-8">
+        <form  ref={form} onSubmit={sendEmail}  className="p-4 px-8 m-auto mt-8">
           <div className="flex flex-col rounded-xl shadow-xl shadow-gray-400">
             <label className="pl-4 text-2xl mb-2">Name</label>
             <input className="text-4l pl-4 ml-2 mr-2 border-solid rounded-[10px] border-2 border-[#001b5e] placeholder:p-4" type="text" name="user_name" />
@@ -37,7 +37,7 @@ const Contact = () => {
             <input className="text-4l pl-4 ml-2 mr-2 border-solid rounded-[10px] border-2 border-[#001b5e]" type="email" name="user_email" />
             <label className="pl-4 text-2xl mb-2">Message</label>
             <textarea name="message" className="text-4l pl-4 resize-none ml-2 mr-2 mb-4 border-solid rounded-[10px] border-2 border-[#001b5e]" cols="30" rows="10"></textarea>
-            <button className="text-2xl text-[#001b5e] rounded-[10px] border-2 border-[#001b5e] m-2 transition-[2s] hover:bg-[#001b5e] hover:text-white md:max-w-[20%]" type="submit" value="Send" >Submit</button>
+            <button onClick={notify} className="text-2xl text-[#001b5e] rounded-[10px] border-2 border-[#001b5e] m-2 transition-[2s] hover:bg-[#001b5e] hover:text-white md:max-w-[20%]" type="submit" value="Send" >Submit</button>
             <ToastContainer />
           </div>
         </form>
